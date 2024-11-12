01bbba817e9e6be20427742f7d50b6c0467296bf
likai0909 <23011764@myrp.edu.sg>
2024-11-12 14:14:38 +0800

    Final submission

--- components/Boats.js
+++ components/Boats.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import {View, Text, Image, ScrollView} from "react-native";
+import {View, Text, Image, ScrollView, StyleSheet} from "react-native";
 import Icon from "react-native-vector-icons/FontAwesome6"
+
+
 
 const Boats = ({icon_name, name, description, picture}) => {
     return (
-        <View>
-            <Text>
-                GetABoat - For Sale
-            </Text>
+        <View style={ {backgroundColor: 'lightblue'}}>
             <View  style={{flexDirection: 'row', alignItems: 'center'}}>
-                <Icon name={icon_name} size={60} color="#B23B23"/>
-                <Text style={{fontSize : 32}}>
+                <Icon name={icon_name} size={60} color="#B23B23"  style={{ marginTop: 40, margin: 20 }} />
+                <Text style={{fontSize : 22, fontWeight: 'bold', fontStyle: 'italic', marginTop:40, marginLeft:10, backgroundColor:'black',
+                    color: 'white', borderWidth: 20, textAlign: 'center', textAlignVertical: 'center', borderRadius: 15}}>
                     {name}
                 </Text>
             </View>
-            <Text>
+            <Text style={{ color: 'black', fontSize: 18, fontStyle: 'italic', marginBottom: 30, marginTop: 30, textAlign: 'center', textAlignVertical: 'center' }}>
                 {description}
             </Text>
-            <Image source={picture} style={{width: 400, height: 400}}/>
+            <View style={{justifyContent:'center', alignItems: 'center'}}>
+                <Image source={picture} style={{width: 300, height: 300, borderRadius: 20}}/>
+            </View>
+
         </View>
     );
 };
@@ -25,6 +28,9 @@ const Boats = ({icon_name, name, description, picture}) => {
 const AllBoats = ()=> {
     return(
         <ScrollView>
+            <Text style={{fontSize:40, fontWeight:'bold', marginTop:50, fontStyle: 'italic',textAlign:'center',  backgroundColor:'black', color: 'white'}}>
+                GetABoat - For Sale !
+            </Text>
             <Boats icon_name = "sailboat"
                    name = "Sea Ray 500 Sundancer"
                    description = "Contoured lines and dramatic styling reveal a refined and powerful presence that will take your breath away"
